c6986d30ba32b935270e874d3c253c036ad20322
Ubuntu <ubuntu@ip-172-31-37-47.ap-northeast-1.compute.internal>
2021-03-25 11:44:29 +0000

    6 edit, update を実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,25 +1,34 @@
 class MessagesController < ApplicationController
     
     def index
-    @messages = Message.all
+      @messages = Message.all
     end
     
     def new
-    @message = Message.new
+      @message = Message.new
     end
     
     def create
-    Message.create(message_params)
+      Message.create(message_params)
     end
     
     def show
-    @message = Message.find(params[:id])
+      @message = Message.find(params[:id])
+    end
+    
+    def edit
+      @message = Message.find(params[:id])
+    end
+    
+    def update
+      message = Message.find(params[:id])
+      message.update(message_params)
+      redirect_to action: :index
     end
     
     
-    private
+  private
     def message_params
-    params.require(:message).permit(:title, :content)
+      params.require(:message).permit(:title, :content) 
     end
-    
 end
