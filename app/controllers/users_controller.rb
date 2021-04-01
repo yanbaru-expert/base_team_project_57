1ad7bf3ce593718bfded80cdd216bcde818a3f7f
makisuteto <ms96program@gmail.com>
2021-04-02 02:07:54 +0900

    編集機能 edit, update を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,18 @@ class UsersController < ApplicationController
     User.create(user_params)
   end
 
-　def show
+  def show
       @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+  end
 
   private
   def user_params
